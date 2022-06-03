ac4fe50890f26588ca636c639ecd1c5c0027fe81
Cameron <cameron.karasiuk@gmail.com>
2022-06-03 12:28:43 -0400

    working server

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+const { notes } = require('./data/db');
 const express = require('express');
 const app = express();
+
+
+app.get('/api/db', (req, res) => {
+    let results = notes;
+    console.log(req.query)
+    res.json(results);
+});
 
 app.listen(3001, () => {
     console.log(`Note-Taker server now on port 3001!`);
